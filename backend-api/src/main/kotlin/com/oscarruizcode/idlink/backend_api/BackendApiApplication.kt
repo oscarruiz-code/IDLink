035ffac434a9f8a9a5ee7dd0b20b7e9844ba051c
oscarruiz-code <oscarruizcode@gmail.com>
2025-07-08 18:47:48 +0200

    PRINCIPIO DE PROYECTO

---
+++ backend-api/src/main/kotlin/com/oscarruizcode/idlink/backend_api/BackendApiApplication.kt
@@ -0,0 +1,11 @@
+package com.oscarruizcode.idlink.backend_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackendApiApplication>(*args)
+}
